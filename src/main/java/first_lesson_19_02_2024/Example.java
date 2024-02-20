b31bfd7cf99ee62734a73eb35af70baace1fe275
ramiz <ramiz90151@gmail.com>
2024-02-20 09:34:59 +0100

    ok

---
+++ src/main/java/first_lesson_19_02_2024/Example.java
@@ -0,0 +1,11 @@
+package first_lesson_19_02_2024;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println(3);
+        System.out.println();
+    }
+}
