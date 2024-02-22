2fb5116344b0c3650da7f8f1794e9265d4f9e46c
ramiz <ramiz90151@gmail.com>
2024-02-22 19:09:32 +0100

    ....

---
+++ src/main/java/lesson_22_02_2022/CalculatorDemo.java
@@ -0,0 +1,11 @@
+package lesson_22_02_2022;
+
+public class CalculatorDemo {
+    public static void main(String[] args) {
+        System.out.println(new Calculator().minusOperation());
+        System.out.println(new Calculator().plusOperation());
+        System.out.println(new Calculator().divisionOperation());
+        System.out.println(new Calculator().multiplicationOperation());
+
+    }
+}
---
+++ src/main/java/lesson_22_02_2022/InputData.java
@@ -0,0 +1,10 @@
+package lesson_22_02_2022;
+
+import java.util.Scanner;
+
+public class InputData {
+    public int input(){
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+}
