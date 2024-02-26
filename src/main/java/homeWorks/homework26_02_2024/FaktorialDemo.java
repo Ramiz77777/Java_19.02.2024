8b5fcea0e39f7cca0ca81896efbb094dc815134f
ramiz <ramiz90151@gmail.com>
2024-02-26 14:01:15 +0100

    ....

---
+++ src/main/java/homeWorks/homework26_02_2024/FaktorialDemo.java
@@ -0,0 +1,10 @@
+package homeWorks.homework26_02_2024;
+
+public class FaktorialDemo {
+    public static void main(String[] args) {
+        FaktorialMethods faktorialMethods = new FaktorialMethods();
+        System.out.println(faktorialMethods.methodWithLoopFor());
+        System.out.println(faktorialMethods.methodWithLoopWhile());
+        System.out.println(faktorialMethods.methodWithDoWhile());
+    }
+}
---
+++ src/main/java/homeWorks/homework26_02_2024/InputData.java
@@ -0,0 +1,11 @@
+package homeWorks.homework26_02_2024;
+
+import java.util.Scanner;
+
+public class InputData {
+    public int InputMethod(){
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        return number;
+    }
+}
