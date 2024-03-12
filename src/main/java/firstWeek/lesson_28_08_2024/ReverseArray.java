f4037939210a1c4f2a8b2c6bfb2ca5b7ee2a2320
ramiz <ramiz90151@gmail.com>
2024-03-12 20:29:52 +0100

    ....

---
+++ src/main/java/firstWeek/lesson_28_08_2024/ReverseArray.java
@@ -0,0 +1,12 @@
+package firstWeek.lesson_28_08_2024;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+
+    }
+//    public int[] Reverse(int[] array){
+//        for (int i = 0; i < array.length; i++) {
+//
+//        }
+//    }
+}
